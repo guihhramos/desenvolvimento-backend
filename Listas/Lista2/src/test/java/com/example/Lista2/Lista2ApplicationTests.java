307f2d8edcdfaf454165085e567384cb78ce7c1a
guihhramos <guiramos902@gmail.com>
2024-08-20 17:11:49 -0300

    Lista 2

---
+++ Listas/Lista2/src/test/java/com/example/Lista2/Lista2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lista2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lista2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
